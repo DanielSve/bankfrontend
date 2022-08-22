5e9a661e4db8dafd20682a02877f5a5bade06aa1
DanielSve <e.danielsvensson@gmail.com>
2022-08-22 16:10:03 +0200

    tested getRequest and displaying users

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import axios from 'axios';
+import React from 'react';
 import './App.css';
 
 function App() {
   const api = axios.create({
-    baseURL:"http://localhost:8080/"
+    baseURL: 'http://localhost:8080/',
   });
 
-  const getAllClients = () => api.get("/clients")
+  const getAllClients = () => api.get('/clients');
 
-  let users = []
+  const [users, setUsers] = React.useState([]);
 
-  const printClients = async () => {
-    users = await getAllClients();
-    console.log(users.data);
-  }
+  const getClients = async () => {
+    const users2 = await getAllClients();
 
-  printClients();
+    setUsers(users2.data);
+  };
 
+  React.useEffect(() => {
+    getClients();
+  }, []);
 
   return (
-    <div className="App">
-      <p>hej</p>
-      {users && users.map((u) => (<div id={u.id}>hej{u.id}</div>))}
+    <div className='App'>
+      
+      {users &&
+        users.map((u) => (
+          <div key={u.id}>
+            <p>{u.firstName}</p>
+            <p>{u.email}</p>
+          </div>
+        ))}
     </div>
   );
 }
